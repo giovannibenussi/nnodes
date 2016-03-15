e2faa62101dae2face001a02c2a28854863cbe2b
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-03-15 03:05:58 -0300

    Genera latitud (entre 0º y 90º) y longitud automaticamente (entre 0º y 180º)

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -3,5 +3,11 @@ class Auto < ActiveRecord::Base
 
     before_create do
         self.marca = "Giovanni"
+        max_latitude = 90
+        min_latitude = 0
+        max_longitude = 90
+        min_longitude = 0
+        self.latitud  = rand * (max_latitude - min_latitude) + min_latitude
+        self.longitud = rand * (max_longitude - min_longitude) + min_longitude
   end
 end
